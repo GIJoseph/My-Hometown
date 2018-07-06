c0e830233822e2d0ff85294f93075a9c9f6ad010
Joseph Goergen <jgoergen15@winona.edu>
2018-07-06 12:45:28 -0500

    added initial ios stuff

---
+++ ios/My Hometown/My Hometown/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  My Hometown
+//
+//  Created by Goergen, Joseph M on 7/6/18.
+//  Copyright © 2018 Goergen, Joseph M. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
